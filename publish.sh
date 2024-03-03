bed3cb310f9015cccce7926627b330641c6b688e
georemo <george.oremo@gmail.com>
2024-03-03 23:03:52 +0300

    set version v0.0.12

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
-Version="v0.0.11"
+Version="v0.0.12"
 # projDir="./cd-user/user.go"
 
 # cd $projDir
 go mod tidy
 git submodule update --remote
+git add cd.go go.mod go.sum ./sys/base/b.go ./sys/base/cd-error.go ./sys/base/go../sys/base/mod go.sum
 git add -A
 git commit -a -m "set version $Version"
 git tag $Version
--- sys/base/b.go
+++ sys/base/b.go
@@ -1,3 +1,4 @@
+// corpdesk base module
 package base
 
 import (
