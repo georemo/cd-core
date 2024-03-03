6cacbfcb8340f590486d08fe4084d9a6f5aca77c
georemo <george.oremo@gmail.com>
2024-03-03 23:08:13 +0300

    set version v0.0.14

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
-Version="v0.0.13"
+Version="v0.0.14"
 # projDir="./cd-user/user.go"
 
 # cd $projDir
 go mod tidy
 git submodule update --remote
-git add cd.go go.mod go.sum sys/base/b.go sys/base/cd-error.go sys/base/go sys/base/go.mod  sys/base/go.sum
+git add cd.go go.mod go.sum sys/base/b.go sys/base/cd-error.go sys/base/go.mod  sys/base/go.sum
 git add -A
 git commit -a -m "set version $Version"
 git tag $Version
