95b396949988810b922c2ee820a1477834b361b0
georemo <george.oremo@gmail.com>
2024-03-04 13:47:35 +0300

    Add package github.com/tcp-x/cd-core/sys/base

--- publish.sh
+++ publish.sh
@@ -11,12 +11,12 @@ git submodule update --remote
 git add sys/base
 git commit -q -am 'Add package github.com/tcp-x/cd-core/sys/base'
 git push -q
-git tag base/$Version
+git tag sys/base/$Version
 # git add cd.go go.mod go.sum sys/base/b.go sys/base/IBase.go sys/base/cd-error.go sys/base/go.mod  sys/base/go.sum sys/user/user.go sys/user/session.go sys/user/go.mod  sys/user/go.sum
 git add sys/user
 git commit -q -am 'Add package github.com/tcp-x/cd-core/sys/user'
 git push -q
-git tag user/$Version
+git tag sys/user/$Version
 
 git add -A
 git commit -a -m "set version $Version"
