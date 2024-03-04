50518b6c75f8f27ee6c73e53f6f334c28cc11e34
georemo <george.oremo@gmail.com>
2024-03-04 13:45:50 +0300

    Add package github.com/tcp-x/cd-core/sys/base

--- publish.sh
+++ publish.sh
@@ -2,17 +2,27 @@
 echo "current repository latest version:\n"
 git ls-remote --tags https://github.com/tcp-x/cd-core.git
 # set latest version
-Version="v0.0.23"
+Version="v0.0.25"
 # projDir="./cd-user/user.go"
 
 # cd $projDir
 go mod tidy
 git submodule update --remote
-git add cd.go go.mod go.sum sys/base/b.go sys/base/IBase.go sys/base/cd-error.go sys/base/go.mod  sys/base/go.sum sys/user/user.go sys/user/session.go sys/user/go.mod  sys/user/go.sum
+git add sys/base
+git commit -q -am 'Add package github.com/tcp-x/cd-core/sys/base'
+git push -q
+git tag base/$Version
+# git add cd.go go.mod go.sum sys/base/b.go sys/base/IBase.go sys/base/cd-error.go sys/base/go.mod  sys/base/go.sum sys/user/user.go sys/user/session.go sys/user/go.mod  sys/user/go.sum
+git add sys/user
+git commit -q -am 'Add package github.com/tcp-x/cd-core/sys/user'
+git push -q
+git tag user/$Version
+
 git add -A
 git commit -a -m "set version $Version"
-git tag $Version
 git push origin $Version
+git tag $Version
+
 
 # cd-cli mod publish 
 
