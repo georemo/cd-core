c16b7365e30c249a50692011cd36ecea88f53184
georemo <george.oremo@gmail.com>
2024-03-04 11:54:10 +0300

    set version v0.0.20

--- publish.sh
+++ publish.sh
@@ -1,10 +1,14 @@
-Version="v0.0.18"
+# get current repository latest version
+echo "current repository latest version:\n"
+git ls-remote --tags https://github.com/tcp-x/cd-core.git
+# set latest version
+Version="v0.0.20"
 # projDir="./cd-user/user.go"
 
 # cd $projDir
 go mod tidy
 git submodule update --remote
-git add cd.go go.mod go.sum sys/base/b.go sys/base/cd-error.go sys/base/go.mod  sys/base/go.sum sys/user/user.go sys/user/session.go sys/user/go.mod  sys/user/go.sum
+git add cd.go go.mod go.sum sys/base/b.go sys/base/IBase.go sys/base/cd-error.go sys/base/go.mod  sys/base/go.sum sys/user/user.go sys/user/session.go sys/user/go.mod  sys/user/go.sum
 git add -A
 git commit -a -m "set version $Version"
 git tag $Version
